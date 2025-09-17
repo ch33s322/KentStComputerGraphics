cc3759524a58c39239ba78ed937b918da34c515e
ch33s322 <61012984+ch33s322@users.noreply.github.com>
2025-09-17 18:28:50 -0400

    submitted version

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,9 @@
 //Dakota Deets september 2025
 #include <GL/glut.h>
 #include <iostream>
-#include <math.h>
+#include <math.h> //only used for sqrt and pow cause im lazy
+#include <Windows.h>
+#include <mmsystem.h>
 
 
 //needs to be before variables because no hoist
@@ -43,6 +45,12 @@ public:
 	}
 
 	vector3d(double a, double b, double c) {
+		x = a;
+		y = b;
+		z = c;
+	}
+
+	void setVector(double a, double b, double c){
 		x = a;
 		y = b;
 		z = c;
@@ -159,6 +167,8 @@ public:
 char drawMode = 'w'; 
 bool drawAxis = true; 
 bool blackAndWhite = false;
+bool music = true;
+bool dancing = false;
 
 int width = 800;
 int height = 600;
@@ -171,6 +181,7 @@ vector3d rightLegColor(0, 0.75, 0);
 vector3d leftLegColor(0, 0.75, 0);
 
 double cameraRadius = 5;
+double danceTheta = 0;
 
 long double theta = 0;
 long double phi = 3.14/2;
@@ -203,13 +214,14 @@ void MyDisplay() {
 	!blackAndWhite ? glColor3f(torsoColor.x, torsoColor.y, torsoColor.z) : glColor3f(saturation, saturation, saturation); // R,G,B
 	box torso(-1, -2.5, -0.5, 2, 2, 1, drawMode);
 
+
 	saturation = sqrt(pow(rightArmColor.x, 2) + pow(rightArmColor.y, 2) + pow(rightArmColor.z, 2));
 	!blackAndWhite ? glColor3f(rightArmColor.x, rightArmColor.y, rightArmColor.z) : glColor3f(saturation, saturation, saturation); // R,G,B
-	box rightArm(-2, -2.5, -0.5, 1, 2, 1, drawMode);
+	box rightArm(-2, (!dancing ? -2.5 : sin(danceTheta)), -0.5, 1, 2, 1, drawMode);
 
 	saturation = sqrt(pow(leftArmColor.x, 2) + pow(leftArmColor.y, 2) + pow(leftArmColor.z, 2));
 	!blackAndWhite ? glColor3f(leftArmColor.x, leftArmColor.y, leftArmColor.z) : glColor3f(saturation, saturation, saturation); // R,G,B
-	box leftArm(1, -2.5, -0.5, 1, 2, 1, drawMode);
+	box leftArm(1, (!dancing ? -2.5 : cos(danceTheta)), -0.5, 1, 2, 1, drawMode);
 
 	saturation = sqrt(pow(rightLegColor.x, 2) + pow(rightLegColor.y, 2) + pow(rightLegColor.z, 2));
 	!blackAndWhite ? glColor3f(rightLegColor.x, rightLegColor.y, rightLegColor.z) : glColor3f(saturation, saturation, saturation); // R,G,B
@@ -224,7 +236,176 @@ void MyDisplay() {
 	glFlush();
 }
 
+void timer(int value) {
+	danceTheta += 0.3;
+	glutPostRedisplay();
+	if (dancing) {
+		glutTimerFunc(16, timer, 0); // Repeat
+	}
+	
+}
 
+void processMenu(int option) {
+	switch (option) {
+	//x rotation cases
+	case 1:  glRotatef(30, 1, 0, 0); break;
+	case 2:  glRotatef(60, 1, 0, 0); break;
+	case 3:  glRotatef(90, 1, 0, 0); break;
+	case 4:  glRotatef(120, 1, 0, 0); break;
+	case 5:  glRotatef(150, 1, 0, 0); break;
+	case 6:  glRotatef(180, 1, 0, 0); break;
+	case 7:  glRotatef(210, 1, 0, 0); break;
+	case 8:  glRotatef(240, 1, 0, 0); break;
+	case 9:  glRotatef(270, 1, 0, 0); break;
+	case 10: glRotatef(300, 1, 0, 0); break;
+	case 11: glRotatef(330, 1, 0, 0); break;
+	case 12: glRotatef(360, 1, 0, 0); break;
+
+	//y rotation cases
+	case 13: glRotatef(30, 0, 1, 0); break;
+	case 14: glRotatef(60, 0, 1, 0); break;
+	case 15: glRotatef(90, 0, 1, 0); break;
+	case 16: glRotatef(120, 0, 1, 0); break;
+	case 17: glRotatef(150, 0, 1, 0); break;
+	case 18: glRotatef(180, 0, 1, 0); break;
+	case 19: glRotatef(210, 0, 1, 0); break;
+	case 20: glRotatef(240, 0, 1, 0); break;
+	case 21: glRotatef(270, 0, 1, 0); break;
+	case 22: glRotatef(300, 0, 1, 0); break;
+	case 23: glRotatef(330, 0, 1, 0); break;
+	case 24: glRotatef(360, 0, 1, 0); break;
+		
+	//z rotation cases
+	case 25: glRotatef(30, 0, 0, 1); break;
+	case 26: glRotatef(60, 0, 0, 1); break;
+	case 27: glRotatef(90, 0, 0, 1); break;
+	case 28: glRotatef(120, 0, 0, 1); break;
+	case 29: glRotatef(150, 0, 0, 1); break;
+	case 30: glRotatef(180, 0, 0, 1); break;
+	case 31: glRotatef(210, 0, 0, 1); break;
+	case 32: glRotatef(240, 0, 0, 1); break;
+	case 33: glRotatef(270, 0, 0, 1); break;
+	case 34: glRotatef(300, 0, 0, 1); break;
+	case 35: glRotatef(330, 0, 0, 1); break;
+	case 36: glRotatef(360, 0, 0, 1); break;
+
+	//projection cases
+	case 37: gluPerspective(100, (double)width / height, 0, 100); break;
+	case 38: glOrtho(-5, 5, -5, 5, -5, 5); break;
+
+	//head color cases
+	case 39: headColor.setVector(.9, 0 ,0); break;
+	case 40: headColor.setVector(0, .8, 0); break;
+	case 41: headColor.setVector(0, 0, .7); break;
+	case 42: headColor.setVector(1, 1, 0); break;
+
+	//arms color cases
+	case 43: rightArmColor.setVector(.9, 0, 0); leftArmColor.setVector(.9, 0, 0);  break;
+	case 44: rightArmColor.setVector(0, 0.8, 0); leftArmColor.setVector(0, 0.8, 0); break;
+	case 45: rightArmColor.setVector(0, 0, 0.7); leftArmColor.setVector(0, 0, 0.7); break;
+	case 46: rightArmColor.setVector(1, 1, 0); leftArmColor.setVector(1, 1, 0); break;
+
+	//torso color cases
+	case 47: torsoColor.setVector(.9, 0, 0); break;
+	case 48: torsoColor.setVector(0, .8, 0); break;
+	case 49: torsoColor.setVector(0, 0, .7); break;
+	case 50: torsoColor.setVector(1, 1, 0); break;
+
+	//legs color cases
+	case 51: rightLegColor.setVector(.9, 0, 0); leftLegColor.setVector(.9, 0, 0); break;
+	case 52: rightLegColor.setVector(0, 0.8, 0); leftLegColor.setVector(0, 0.8, 0); break;
+	case 53: rightLegColor.setVector(0, 0, 0.7); leftLegColor.setVector(0, 0, 0.7); break;
+	case 54: rightLegColor.setVector(1, 1, 0); leftLegColor.setVector(1, 1, 0); break;
+	}
+
+	glutPostRedisplay();
+}
+//unfortunately no hoist makes this gross
+class menu {
+public:
+	menu() {
+		int xRotateMenu = glutCreateMenu(processMenu);
+		glutAddMenuEntry("30", 1);
+		glutAddMenuEntry("60", 2);
+		glutAddMenuEntry("90", 3);
+		glutAddMenuEntry("120", 4);
+		glutAddMenuEntry("150", 5);
+		glutAddMenuEntry("180", 6);
+		glutAddMenuEntry("210", 7);
+		glutAddMenuEntry("240", 8);
+		glutAddMenuEntry("270", 9);
+		glutAddMenuEntry("300", 10);
+		glutAddMenuEntry("330", 11);
+		glutAddMenuEntry("360", 12);
+
+		int yRotateMenu = glutCreateMenu(processMenu);
+		glutAddMenuEntry("30", 13);
+		glutAddMenuEntry("60", 14);
+		glutAddMenuEntry("90", 15);
+		glutAddMenuEntry("120", 16);
+		glutAddMenuEntry("150", 17);
+		glutAddMenuEntry("180", 18);
+		glutAddMenuEntry("210", 19);
+		glutAddMenuEntry("240", 20);
+		glutAddMenuEntry("270", 21);
+		glutAddMenuEntry("300", 22);
+		glutAddMenuEntry("330", 23);
+		glutAddMenuEntry("360", 24);
+
+		int zRotateMenu = glutCreateMenu(processMenu);
+		glutAddMenuEntry("30", 25);
+		glutAddMenuEntry("60", 26);
+		glutAddMenuEntry("90", 27);
+		glutAddMenuEntry("120", 28);
+		glutAddMenuEntry("150", 29);
+		glutAddMenuEntry("180", 30);
+		glutAddMenuEntry("210", 31);
+		glutAddMenuEntry("240", 32);
+		glutAddMenuEntry("270", 33);
+		glutAddMenuEntry("300", 34);
+		glutAddMenuEntry("330", 35);
+		glutAddMenuEntry("360", 36);
+
+		int projection = glutCreateMenu(processMenu);
+		glutAddMenuEntry("Orthographic", 37);
+		glutAddMenuEntry("Perspective", 38);
+		
+		int headColor = glutCreateMenu(processMenu);
+		glutAddMenuEntry("red", 39);
+		glutAddMenuEntry("green", 40);
+		glutAddMenuEntry("blue", 41);
+		glutAddMenuEntry("yellow", 42);
+
+		int armsColor = glutCreateMenu(processMenu);
+		glutAddMenuEntry("red", 43);
+		glutAddMenuEntry("green", 44);
+		glutAddMenuEntry("blue", 45);
+		glutAddMenuEntry("yellow", 46);
+
+		int torsoColor = glutCreateMenu(processMenu);
+		glutAddMenuEntry("red", 47);
+		glutAddMenuEntry("green", 48);
+		glutAddMenuEntry("blue", 49);
+		glutAddMenuEntry("yellow", 50);
+
+		int legsColor = glutCreateMenu(processMenu);
+		glutAddMenuEntry("red", 51);
+		glutAddMenuEntry("green", 52);
+		glutAddMenuEntry("blue", 53);
+		glutAddMenuEntry("yellow", 54);
+
+
+		int menu = glutCreateMenu(processMenu);
+		glutAddSubMenu("X rotation", xRotateMenu);
+		glutAddSubMenu("Y rotation", yRotateMenu);
+		glutAddSubMenu("Z rotation", zRotateMenu);
+		glutAddSubMenu("Projection type", projection);
+		glutAddSubMenu("Head color", headColor);
+		glutAddSubMenu("Torso color", torsoColor);
+		glutAddSubMenu("Arms color", armsColor);
+		glutAddSubMenu("Legs color", legsColor);
+	}
+};
 
 void processKeys(unsigned char key, int x, int y) {
 	switch (key) {
@@ -238,7 +419,6 @@ void processKeys(unsigned char key, int x, int y) {
 		drawMode = 's';
 		break;
 	case 'c':
-		//implement clearing screen and only sxowing background
 		drawMode = 'c';
 		drawAxis = false;
 		break;
@@ -246,8 +426,26 @@ void processKeys(unsigned char key, int x, int y) {
 		drawAxis = !drawAxis;
 		break;
 	case 't':
-		//implement speaking functionality
+		//TALKING BONUS SECTION
+		//TALKING BONUS SECTION
+		//TALKING BONUS SECTION
+		PlaySoundW(TEXT("speech.wav"), NULL, SND_FILENAME | SND_ASYNC);
 		std::cout << "Robot speaking" << std::endl;
+		break;
+
+	case 'd':
+		dancing = !dancing;
+		if (music) {
+			PlaySoundW(TEXT("dance.wav"), NULL, SND_FILENAME | SND_ASYNC);
+		}
+		else {
+			PlaySoundW(NULL, NULL, SND_FILENAME | SND_ASYNC);
+		}
+		timer(0);
+		break;
+	case 'm':
+		music = !music;
+		music ? NULL : PlaySoundW(NULL, NULL, SND_FILENAME | SND_ASYNC);
 		break;
 	case 'i':
 		//rotate camera
@@ -292,7 +490,7 @@ void processMouse(int button, int state, int x, int y) {
 		blackAndWhite = !blackAndWhite;
 	}
 	else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
-
+		glutAttachMenu(GLUT_RIGHT_BUTTON);
 	}
 	else if (button == 3) { //scroll up
 		cameraRadius -= .1; 
@@ -332,21 +530,27 @@ int main(int argc, char** argv) {
 
 	
 	gluLookAt(cameraX, cameraY, cameraZ, 0, 0, 0, 0, 1, 0);
-
+	
+	
+	menu a;
 
 	std::cout << "p: vertes/point only mode\n"
 		<< "w: wireframe mode\n"
 		<< "s: surface mode\n"
 		<< "a: toggle display axis\n"
 		<< "c: clear the screen\n"
-		<< "Right Mouse: display menu\n"
+		<< "Right Mouse: opens right click menu\n"
 		<< "Left Mouse: toggle BW and Color\n"
 		<< "\n\nCAMERA CONTROLLS: (press or hold)\n"
 		<< "i: rotate camera up\n"
 		<< "j: rotate camera left\n"
 		<< "k: rotate camera right\n"
 		<< "l: rotate camera left\n"
-		<< "SCROLL WHEEL: zoom in and out\n";
+		<< "SCROLL WHEEL: zoom in and out\n\n\n"
+		//TALKING BONUS SECTION
+		<< "BONUS!!!\n"
+		<< "t: speak\n"
+		<< "d: dance\n";
 
 	glutDisplayFunc(MyDisplay); // call the drawing function
 	glutKeyboardFunc(processKeys);
